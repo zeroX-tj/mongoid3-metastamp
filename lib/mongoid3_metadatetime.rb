fa51a3e5fbb46fbd3baf9e7efd175df471c77890
Niels Vandekeybus <nvdk@arguslabs.be>
2012-08-14 12:18:29 +0200

    moved file

--- lib/mongoid3_metadatetime.rb
+++ lib/mongoid3_metadatetime.rb
@@ -1,5 +1,5 @@
 require "mongoid3_metadatetime/version"
-require "mongoid3_metadatetime/meta_data_time"
+require "mongoid3_metadatetime/mongoid3/meta_data_time"
 
 module Mongoid3
   module Metadatetime
