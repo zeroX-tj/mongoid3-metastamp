1463643b897f63eac3de8339b82862e9dc04261d
Niels V <nvdk@arguslabs.be>
2012-08-14 14:58:35 +0200

    mongoize should be class functions, use ::Time instead of Time

--- lib/mongoid3/metastamp/time.rb
+++ lib/mongoid3/metastamp/time.rb
@@ -1,39 +1,42 @@
 # (c) 2012 Wep'IT bvba
 module Mongoid3::MetaStamp
-  class Time < Time
+  class Time < ::Time
     def mongoize
-        { 
-          time:         self,
-          year:         self.year,
-          month:        self.month,
-          day:          self.day,
-          wday:         self.wday,
-          hour:         self.hour,
-          min:          self.min,
-          sec:          self.sec,
-          zone:         self.zone,
-          offset:       self.utc_offset
-        }.stringify_keys
+      { 
+        time:         self,
+        year:         self.year,
+        month:        self.month,
+        day:          self.day,
+        wday:         self.wday,
+        hour:         self.hour,
+        min:          self.min,
+        sec:          self.sec,
+        zone:         self.zone,
+        offset:       self.utc_offset
+      }.stringify_keys
     end
     
-    class << self
-      def mongoize(object)
-        case object
-        when Mongoid3::MetaStamp::Time then object.mongoize
-        when DateTime then Mongoid3::MetaStamp::Time.parse(object.to_s).mongoize
-        else object
-        end
+    
+    def self.mongoize(object)
+      return nil if object.blank?
+      if object.is_a?(Mongoid3::MetaStamp::Time)
+        object.mongoize
+      elsif object.is_a?(::Time) || object.is_a?(::DateTime)
+        Mongoid3::MetaStamp::Time.parse(object.to_s).mongoize
+      else 
+        object
       end
+    end
       
-      def demongoize(object)
-        Mongoid3::MetaStamp::Time.parse(object.to_s)
-      end
+    def self.demongoize(object)
+      return nil if object.blank?
+      Mongoid3::MetaStamp::Time.parse(object.to_s)
+    end
       
-      def evolve(object)
-        case object
-        when Mongoid3::MetaStamp::Time then object.mongoize
-        else object
-        end
+    def self.evolve(object)
+      case object
+      when Mongoid3::MetaStamp::Time then object.mongoize
+      else object
       end
     end
   end
--- lib/mongoid3/metastamp/version.rb
+++ lib/mongoid3/metastamp/version.rb
@@ -1,5 +1,5 @@
 module Mongoid3
   module MetaStamp
-    VERSION = "0.0.3"
+    VERSION = "0.0.5"
   end
 end
